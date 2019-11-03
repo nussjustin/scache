40d14697c7c500a7648f64604003516efb8a84f0
Justin Nuß <nuss.justin@gmail.com>
2019-11-03 09:35:15 +0100

    Update Go 1.14 hasher to hash/maphash

--- hash_go114.go
+++ hash_go114.go
@@ -3,22 +3,22 @@
 package scache
 
 import (
-	"bytes/hash"
+	"hash/maphash"
 	"math/rand"
 )
 
 type hasher struct {
-	seed hash.Seed
+	seed maphash.Seed
 }
 
 func newHasher() hasher {
 	return hasher{
-		seed: hash.MakeSeed(rand.Uint64()),
+		seed: maphash.MakeSeed(rand.Uint64()),
 	}
 }
 
 func (h hasher) hash(s string) uint64 {
-	var sh hash.Hash
+	var sh maphash.Hash
 	sh.SetSeed(h.seed)
 	sh.AddString(s)
 	return sh.Sum64()
