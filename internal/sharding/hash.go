9b5f692f407bddad57677166a813a080e4e3925a
Justin Nuß <nuss.justin@gmail.com>
2019-11-09 07:38:00 +0100

    Fix sharding.Hasher implementation for Go 1.13

--- internal/sharding/hash.go
+++ internal/sharding/hash.go
@@ -10,7 +10,7 @@ func NewHasher() Hasher {
 	return Hasher{}
 }
 
-func (h Hasher) Hasher(s string) uint64 {
+func (h Hasher) Hash(s string) uint64 {
 	f := fnv.New32()
 	_, _ = f.Write([]byte(s))
 	return uint64(f.Sum32())
