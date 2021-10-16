5554eb8b32a588973e3015cd426b6c1551af9ed3
Justin Nuß <nuss.justin@gmail.com>
2021-10-16 13:28:46 +0200

    Add parallel lookup tests

--- lookup/bench_test.go
+++ lookup/bench_test.go
@@ -33,3 +33,32 @@ func BenchmarkLookup(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkLookupParallel(b *testing.B) {
+	b.Run("Hit", func(b *testing.B) {
+		c := lookup.NewCache[string](newMapCache[string](map[string]string{"hit": "hit"}), func(ctx context.Context, key mem.RO) (val string, err error) {
+			return "", lookup.ErrSkip
+		}, nil)
+
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				c.Get(context.Background(), mem.S("hit"))
+			}
+		})
+	})
+
+	b.Run("Miss", func(b *testing.B) {
+		// Syntax "var nc scache.Noop[string]" currently causes an error because the "[" can not be parsed
+		nc := scache.Noop[string]{}
+
+		c := lookup.NewCache[string](nc, func(ctx context.Context, key mem.RO) (val string, err error) {
+			return "", lookup.ErrSkip
+		}, nil)
+
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				c.Get(context.Background(), mem.S("miss"))
+			}
+		})
+	})
+}
